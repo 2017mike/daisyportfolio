c10325d0634d69e4401cc6c54b1b96e931ba4488
Michael Scharf <mwscharf@ucdavis.edu>
2026-01-28 20:16:21 -0800

    remove zap for now

--- info/index.ts
+++ info/index.ts
@@ -6,14 +6,15 @@ const info = {
   github: "https://github.com/2017mike",
   email: "mwscharf@ucdavis.edu",
   sections: ["About", "Projects", "Contact"],
-  featuredProjects: [{
-    name: 'Zap',
-    summary: "A bug-tracking app that aims to combine project management and bug tracking aspects of app development.",
-    image: '/zapMock.png', 
-    link: "https://zap-app-646e95e3d663.herokuapp.com/",
-    github: "https://github.com/2017mike/zapApp",
-    technologies: ["React", "Node.js", "Express", "MongoDB", "Material UI"]
-  },
+  featuredProjects: [
+  //   {
+  //   name: 'Zap',
+  //   summary: "A bug-tracking app that aims to combine project management and bug tracking aspects of app development.",
+  //   image: '/zapMock.png', 
+  //   link: "https://zap-app-646e95e3d663.herokuapp.com/",
+  //   github: "https://github.com/2017mike/zapApp",
+  //   technologies: ["React", "Node.js", "Express", "MongoDB", "Material UI"]
+  // },
   {
     name: 'tabAngel.io',
     summary: "A website where users can submit and view guitar tabs. Includes a guitar tab creator that automatically formats submissions.",
